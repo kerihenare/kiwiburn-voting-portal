231a8f47cd88962f19f189243effa24ce71738f7
Keri Henare <keri@henare.co.nz>
2026-03-17 22:06:14 +1300

    refactor: convert timer-badge.tsx to props conventions

--- components/timer-badge.tsx
+++ components/timer-badge.tsx
@@ -13,39 +13,41 @@ interface TimerBadgeProps {
   closesAt: Date
 }
 
-export function TimerBadge({ opensAt, closesAt }: TimerBadgeProps) {
+export function TimerBadge(props: TimerBadgeProps) {
   const now = new Date()
 
-  if (isFuture(opensAt)) {
+  if (isFuture(props.opensAt)) {
     return (
       <Tooltip>
         <TooltipTrigger asChild>
           <Badge className="bg-blue-100 text-blue-800" variant="secondary">
-            Opens in {formatDistanceToNow(opensAt)}
+            Opens in {formatDistanceToNow(props.opensAt)}
           </Badge>
         </TooltipTrigger>
-        <TooltipContent>{format(opensAt, "d MMM yyyy, h:mm a")}</TooltipContent>
+        <TooltipContent>
+          {format(props.opensAt, "d MMM yyyy, h:mm a")}
+        </TooltipContent>
       </Tooltip>
     )
   }
 
-  if (isPast(closesAt)) {
+  if (isPast(props.closesAt)) {
     return (
       <Tooltip>
         <TooltipTrigger asChild>
           <Badge className="bg-gray-100 text-gray-600" variant="secondary">
-            Closed {formatDistanceToNow(closesAt, { addSuffix: true })}
+            Closed {formatDistanceToNow(props.closesAt, { addSuffix: true })}
           </Badge>
         </TooltipTrigger>
         <TooltipContent>
-          {format(closesAt, "d MMM yyyy, h:mm a")}
+          {format(props.closesAt, "d MMM yyyy, h:mm a")}
         </TooltipContent>
       </Tooltip>
     )
   }
 
   // Currently open
-  const msRemaining = closesAt.getTime() - now.getTime()
+  const msRemaining = props.closesAt.getTime() - now.getTime()
   const hoursRemaining = msRemaining / (1000 * 60 * 60)
 
   if (hoursRemaining < 1) {
@@ -55,7 +57,7 @@ export function TimerBadge({ opensAt, closesAt }: TimerBadgeProps) {
           <Badge variant="destructive">Closes in {"< 1 minute"}</Badge>
         </TooltipTrigger>
         <TooltipContent>
-          {format(closesAt, "d MMM yyyy, h:mm a")}
+          {format(props.closesAt, "d MMM yyyy, h:mm a")}
         </TooltipContent>
       </Tooltip>
     )
@@ -72,10 +74,12 @@ export function TimerBadge({ opensAt, closesAt }: TimerBadgeProps) {
           }
           variant="secondary"
         >
-          Closes in {formatDistanceToNow(closesAt)}
+          Closes in {formatDistanceToNow(props.closesAt)}
         </Badge>
       </TooltipTrigger>
-      <TooltipContent>{format(closesAt, "d MMM yyyy, h:mm a")}</TooltipContent>
+      <TooltipContent>
+        {format(props.closesAt, "d MMM yyyy, h:mm a")}
+      </TooltipContent>
     </Tooltip>
   )
 }
