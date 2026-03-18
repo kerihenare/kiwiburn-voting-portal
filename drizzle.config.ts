9d0507761d9a632b4986dfc72702b2e41856c77a
Keri Henare <keri@henare.co.nz>
2026-03-18 22:46:56 +1300

    Env var

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "drizzle-kit"
 
 export default defineConfig({
   dbCredentials: {
-    url: process.env.DATABASE_URL!,
+    url: process.env.POSTGRES_URL!,
   },
   dialect: "postgresql",
   out: "./drizzle",
